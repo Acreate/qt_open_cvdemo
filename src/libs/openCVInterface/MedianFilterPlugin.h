23a48d92ddaf11be9a598508185b3cd4dc185547
mrc <1552397900@qq.com>
2023-02-23 00:02:08 +0800

    首次提交

---
+++ src/libs/openCVInterface/MedianFilterPlugin.h
@@ -0,0 +1,26 @@
+﻿#ifndef MEDIANFILTERPLUGIN_H_H_HEAD__FILE__
+#define MEDIANFILTERPLUGIN_H_H_HEAD__FILE__
+#pragma once
+
+#include <qobject.h>
+#include "CvPluginInterface.h"
+
+namespace openCVInterface {
+	class OPENCVINTERFACE_EXPORT MedianFilterPlugin : public QObject, public CvPluginInterface {
+		Q_OBJECT
+		Q_PLUGIN_METADATA(IID  "com.amin.cvplugininterfce")
+		Q_INTERFACES(openCVInterface::CvPluginInterface)
+
+	public:
+		explicit MedianFilterPlugin( QObject *parent = nullptr );
+
+		~MedianFilterPlugin( ) override;
+
+		QString description( ) override;
+
+		void processImage( const cv::Mat &inputImage, cv::Mat &outputImage ) override;
+	};
+}
+
+
+#endif // MEDIANFILTERPLUGIN_H_H_HEAD__FILE__
